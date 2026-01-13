f1da886f2f55f2bd23a14ce15b8fb823909dd99c
Joosep Jääger <joosep.jaager@iohk.io>
2026-01-13 17:59:32 +0200

    switch to GADTs

--- src/Test/AntiGen.hs
+++ src/Test/AntiGen.hs
@@ -14,46 +14,56 @@ module Test.AntiGen (
   sometimes,
   countDecisionPoints,
   evalToPartial,
+  genZap,
 ) where
 
+import Control.Monad ((<=<))
 import Control.Monad.Free.Church (F (..), MonadFree (..))
+import Control.Monad.Free.Class (wrapT)
 import Test.QuickCheck (Gen)
 import Test.QuickCheck.GenT (GenT (..), MonadGen (..), runGenT)
 
-data BiGen t next = BiGen (Gen t) (Maybe (Gen t)) (t -> next)
-  deriving (Functor)
+data BiGen next where
+  BiGen :: Gen t -> Maybe (Gen t) -> (t -> next) -> BiGen next
 
-newtype AntiGen t a = AntiGen (F (BiGen t) a)
+instance Functor BiGen where
+  fmap f (BiGen p n c) = BiGen p n $ f . c
+
+newtype AntiGen a = AntiGen (F BiGen a)
   deriving (Functor, Applicative, Monad)
 
-always :: Gen a -> AntiGen a a
+always :: Gen a -> AntiGen a
 always g = AntiGen $ F $ \p b -> b $ BiGen g Nothing p
 
-sometimes :: Gen a -> Gen a -> AntiGen a a
+sometimes :: Gen a -> Gen a -> AntiGen a
 sometimes pos neg = AntiGen $ F $ \p b -> b $ BiGen pos (Just neg) p
 
-data DecisionPoint t next = DecisionPoint
-  { dpValue :: t
-  , dpPositiveGen :: Gen t
-  , dpNegativeGen :: Gen t
-  , dpContinuation :: t -> next
-  }
-  deriving (Functor)
+data DecisionPoint next where
+  DecisionPoint ::
+    { dpValue :: t
+    , dpPositiveGen :: Gen t
+    , dpNegativeGen :: Gen t
+    , dpContinuation :: t -> next
+    } ->
+    DecisionPoint next
 
-continue :: DecisionPoint t next -> next
+instance Functor DecisionPoint where
+  fmap f (DecisionPoint v p n c) = DecisionPoint v p n $ f . c
+
+continue :: DecisionPoint next -> next
 continue DecisionPoint {..} = dpContinuation dpValue
 
-newtype PartialGen t a = PartialGen (F (DecisionPoint t) a)
-  deriving (Functor, Applicative, Monad, MonadFree (DecisionPoint t))
+newtype PartialGen t a = PartialGen (F DecisionPoint a)
+  deriving (Functor, Applicative, Monad, MonadFree DecisionPoint)
 
 wrapGenT :: (MonadFree f m, Functor f) => f (GenT m a) -> GenT m a
 wrapGenT m = GenT $ \g s -> wrap $ (\(GenT f) -> f g s) <$> m
 
-evalToPartial :: AntiGen t a -> Gen (PartialGen t a)
+evalToPartial :: AntiGen a -> Gen (PartialGen t a)
 evalToPartial (AntiGen (F m)) = runGenT $ m pure $ \(BiGen pos mNeg c) -> do
   value <- liftGen pos
   case mNeg of
-    Just neg -> wrapGenT $ DecisionPoint value pos neg c
+    Just neg -> wrapT $ DecisionPoint value pos neg c
     Nothing -> c value
 
 countDecisionPoints :: PartialGen t a -> Int
@@ -71,12 +81,21 @@ zap p
       cutoffDepth <- choose (0, maxDepth - 1)
       let
         go :: PartialGen t a -> Int -> Gen (PartialGen t a)
-        go (PartialGen (F m)) 0 = m undefined undefined
-        go (PartialGen (F m)) d = m (\x -> go (pure x) d) $ \dp -> do
+        go (PartialGen (F m)) 0 = m (pure . pure) $ \DecisionPoint {..} -> do
+          regenerate =<< dpContinuation =<< dpNegativeGen
+        go (PartialGen (F m)) d = m (pure . pure) $ \dp -> do
           c <- continue dp
           go c (d - 1)
       go p cutoffDepth
   | otherwise = pure p
 
+zapNTimes :: Int -> PartialGen t a -> Gen (PartialGen t a)
+zapNTimes n x
+  | n <= 0 = pure x
+  | otherwise = zapNTimes (n - 1) =<< zap x
+
 evalPartial :: PartialGen t a -> a
 evalPartial (PartialGen (F m)) = m id continue
+
+genZap :: Int -> AntiGen a -> Gen a
+genZap n = fmap evalPartial <$> zapNTimes n <=< evalToPartial
