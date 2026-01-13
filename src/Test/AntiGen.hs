a17af667646caf6b5f484f6ec23cf5aba2c5d83c
Joosep Jääger <joosep.jaager@iohk.io>
2026-01-13 17:20:28 +0200

    Added tests

--- src/Test/AntiGen.hs
+++ src/Test/AntiGen.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Test.AntiGen (
   AntiGen,
   always,
   sometimes,
+  countDecisionPoints,
+  evalToPartial,
 ) where
 
 import Control.Monad.Free.Church (F (..), MonadFree (..))
@@ -30,28 +34,49 @@ sometimes pos neg = AntiGen $ F $ \p b -> b $ BiGen pos (Just neg) p
 
 data DecisionPoint t next = DecisionPoint
   { dpValue :: t
+  , dpPositiveGen :: Gen t
   , dpNegativeGen :: Gen t
   , dpContinuation :: t -> next
   }
   deriving (Functor)
 
+continue :: DecisionPoint t next -> next
+continue DecisionPoint {..} = dpContinuation dpValue
+
 newtype PartialGen t a = PartialGen (F (DecisionPoint t) a)
   deriving (Functor, Applicative, Monad, MonadFree (DecisionPoint t))
 
-evalToSpec :: AntiGen t a -> Gen (PartialGen t a)
-evalToSpec (AntiGen (F m)) = runGenT $ m pure $ \(BiGen pos mNeg c) -> do
-  x <- liftGen pos
+wrapGenT :: (MonadFree f m, Functor f) => f (GenT m a) -> GenT m a
+wrapGenT m = GenT $ \g s -> wrap $ (\(GenT f) -> f g s) <$> m
+
+evalToPartial :: AntiGen t a -> Gen (PartialGen t a)
+evalToPartial (AntiGen (F m)) = runGenT $ m pure $ \(BiGen pos mNeg c) -> do
+  value <- liftGen pos
   case mNeg of
-    Just neg -> GenT $ \g s -> PartialGen $ F $ \p b ->
-      b $
-        DecisionPoint x neg $
-          (\(GenT f) -> let PartialGen (F m') = f g s in m' p b)
-            . c
-    Nothing -> c x
+    Just neg -> wrapGenT $ DecisionPoint value pos neg c
+    Nothing -> c value
 
 countDecisionPoints :: PartialGen t a -> Int
-countDecisionPoints (PartialGen (F m)) = m (const 0) $ \DecisionPoint {..} ->
-  1 + dpContinuation dpValue
+countDecisionPoints (PartialGen (F m)) = m (const 0) $ succ . continue
+
+regenerate :: PartialGen t a -> Gen (PartialGen t a)
+regenerate (PartialGen (F m)) = runGenT $ m pure $ \(DecisionPoint {..}) -> do
+  value <- liftGen dpPositiveGen
+  wrapGenT $ DecisionPoint value dpPositiveGen dpNegativeGen dpContinuation
+
+zap :: PartialGen t a -> Gen (PartialGen t a)
+zap p
+  | let maxDepth = countDecisionPoints p
+  , maxDepth > 0 = do
+      cutoffDepth <- choose (0, maxDepth - 1)
+      let
+        go :: PartialGen t a -> Int -> Gen (PartialGen t a)
+        go (PartialGen (F m)) 0 = m undefined undefined
+        go (PartialGen (F m)) d = m (\x -> go (pure x) d) $ \dp -> do
+          c <- continue dp
+          go c (d - 1)
+      go p cutoffDepth
+  | otherwise = pure p
 
 evalPartial :: PartialGen t a -> a
-evalPartial (PartialGen (F m)) = m id $ \DecisionPoint {..} -> dpContinuation dpValue
+evalPartial (PartialGen (F m)) = m id continue
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main (main) where
 
+import Test.AntiGen (countDecisionPoints, evalToPartial, sometimes)
+import Test.Hspec (describe, hspec, shouldBe)
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck (Arbitrary (..), NonPositive (..), Positive (..), (.&&.), (===))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+  describe "AntiGen" $ do
+    describe "treeDepth" $ do
+      prop "pure has depth of zero" $ do
+        let
+          m = pure ()
+        pt <- evalToPartial m
+        pure $ countDecisionPoints pt `shouldBe` 0
+      prop "single bind has depth of one, right identity holds" $ do
+        let
+          g = (getPositive @Int <$> arbitrary) `sometimes` (getNonPositive <$> arbitrary)
+          m = return =<< g
+          m' = g
+        pt <- evalToPartial m
+        pt' <- evalToPartial m'
+        pure $ countDecisionPoints pt === countDecisionPoints pt' .&&. countDecisionPoints pt === 1
